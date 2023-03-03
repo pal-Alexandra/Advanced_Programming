0c4d6b1ccf450cc7612f4e96ad313ec3f736c790
Pal Alexandra <100137533+pal-Alexandra@users.noreply.github.com>
2023-03-03 13:11:42 +0200

    Delete Laboratory2/src/laboratory2 directory

--- Laboratory2/src/laboratory2/LocationType.java
+++
@@ -1,8 +0,0 @@
-package laboratory2;
-
-/**
- * This enum describes the types that a location can have
- */
-public enum LocationType {
-    CITY, AIRPORT, GAS_STATION
-}
--- Laboratory2/src/laboratory2/RoadType.java
+++
@@ -1,8 +0,0 @@
-package laboratory2;
-
-/**
- * This enum describes the types that a road can have
- */
-public enum RoadType {
-    HIGHWAY, EXPRESS, COUNTRY
-}
