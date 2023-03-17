879bb71af62ab211ac4ddfd5dea83e426a558fb6
Pal Alexandra <100137533+pal-Alexandra@users.noreply.github.com>
2023-03-17 09:23:17 +0200

    Add files via upload

---
+++ Homework3/homework3/CompanyPosition.java
@@ -0,0 +1,9 @@
+package homework3;
+
+/**
+ * @author Pal Alexandra
+ * This enum describes the positions a person could occupy in a company.
+ */
+public enum CompanyPosition {
+    PROGRAMMER, DESIGNER, MANAGER, TEAM_LEADER
+}
